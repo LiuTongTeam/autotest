ad7b38240ad4232ee3c8167cecf52002ec7a5566
shenqingyan <shenqingyan@up-tech.cn>
2019-08-08 18:00:07 +0800

    主流程测试case--登陆、注册及身份认证

--- src/main/java/io/cex/test/autotest/interfacecase/cex/BaseCase.java
+++ src/main/java/io/cex/test/autotest/interfacecase/cex/BaseCase.java
@@ -3,14 +3,15 @@ package io.cex.test.autotest.interfacecase.cex;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import io.cex.test.framework.common.FileUtil;
 import io.cex.test.framework.common.RandomUtil;
 import io.cex.test.framework.httputil.OkHttpClientManager;
 import io.cex.test.framework.jsonutil.JsonFileUtil;
 import lombok.extern.slf4j.Slf4j;
-import netscape.javascript.JSException;
-import netscape.javascript.JSObject;
 import okhttp3.Response;
+import org.apache.poi.openxml4j.opc.PackagingURIHelper;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 
@@ -18,19 +19,33 @@ import java.util.HashMap;
 public class BaseCase {
     //测试环境信息
     public static final String ip = "http://139.9.55.125/apis";
+    //数据库连接信息
     public static final String mysql = "jdbc:mysql://172.29.19.71:3306/cex?useUnicode=true&characterEncoding=UTF8&user=root&password=48rm@hd2o3EX";
+    //http header
     public static final HashMap header = new HashMap<String, String>();
-    public static final String lang = "en-US";
     public static JSONObject jsonbody = new JSONObject();
+    //接口参数
     public static final String randomPhone = RandomUtil.getRandomPhoneNum();
+    public static final String pwd = "Aa123456";
+    public static final String area = "86";
+    public static final String lang = "en-US";
+    public static final String countryId = "40";
+    public static final String certificateType = "0";
+    public static final String DEVICESOURCE = "native";
+    public static final String DEVICEID = "A5A6F0c6B90638A2F-e195d43830A5e9979906e5A0A8A-9330A0B3ADBBB9d93-AFF5dBcF9-A4c749-AB10-4EB49EABF9E7-85315174-34961239";
+    //认证图片路径
+    public static final String fileUrl = "http://172.29.16.161/";
+    //接口url
     public static final String loginUrl = "/user/login";
     public static final String registerUrl = "/user/register";
+    public static final String upLoadFileUrl = "/user/file/upload/file";
+    public static final String identityUrl = "/user/authenticate/submit/identity";
     /**
     * @desc 数据初始化
     **/
     public static void dataInit(){
-        header.put("DEVICEID", "A5A6F0c6B90638A2F-e195d43830A5e9979906e5A0A8A-9330A0B3ADBBB9d93-AFF5dBcF9-A4c749-AB10-4EB49EABF9E7-85315174-34961239");
-        header.put("DEVICESOURCE","native");
+        header.put("DEVICEID",DEVICEID);
+        header.put("DEVICESOURCE",DEVICESOURCE);
         header.put("Lang",lang);
         jsonbody.put("lang",lang);
     }
@@ -40,6 +55,7 @@ public class BaseCase {
      * @param user 用户名
      * @param pwd 密码
      * @param area 区号
+     * @return token
      **/
     public static String userLogin(String user,String pwd,String area){
         dataInit();
@@ -49,20 +65,20 @@ public class BaseCase {
         object.put("mobileArea",area);
         object.put("verifyCode","111111");
         jsonbody.put("data",object);
-        System.out.printf(jsonbody.toJSONString()+"\n");
         try {
             Response response = OkHttpClientManager.post(ip+loginUrl, jsonbody.toJSONString(),
                     "application/json", header);
             if (response.code()==200){
                 JSONObject rspjson = JSON.parseObject(response.body().string());
                 if (rspjson.get("code").equals("000000")){
+                    log.info("-------------Login success"+"body:"+rspjson.toJSONString()+"\n");
                     return JsonFileUtil.jsonToMap(rspjson,new HashMap<String, Object>()).get("token").toString();
                 }else {
-                    log.error("----------------login failed, trace id is:"+rspjson.get("traceID")+"\n");
+                    log.error("----------------login failed, trace id is:"+rspjson.get("traceId")+"\n");
                     return null;
                 }
             }else {
-                log.error("----------------Server connect failed"+response.body());
+                log.error("----------------Server connect failed"+response.body()+"\n");
                 return null;
             }
         }catch (IOException e){
@@ -86,22 +102,63 @@ public class BaseCase {
         object.put("mobileArea",area);
         object.put("verifyCode","123456");
         jsonbody.put("data",object);
-        System.out.printf(jsonbody.toJSONString()+"\n");
         try {
             Response response = OkHttpClientManager.post(ip+registerUrl, jsonbody.toJSONString(),
                     "application/json", header);
             if (response.code()==200){
                 JSONObject rspjson = JSON.parseObject(response.body().string());
                 if (rspjson.get("code").equals("000000")){
-                    log.info("-------------Regist success");
+                    log.info("-------------Regist success"+"body:"+rspjson.toJSONString()+"\n");
                 }else {
-                    log.error("----------------login failed, trace id is:"+rspjson.get("traceID")+"\n");
+                    log.error("----------------login failed, trace id is:"+rspjson.get("traceId")+"\n");
             }
             }else {
-                log.error("----------------Server connect failed"+response.body());
+                log.error("----------------Server connect failed"+response.body()+"\n");
             }
         }catch (IOException e){
             e.printStackTrace();
         }
+    }
+    /**
+    * @desc 上传文件
+    * @param  fileUrl 文件服务器地址
+     * @param fileName 图片名，带文件后缀，如test.jpeg
+     * @param token 登陆token
+     * @return 图片ID
+    **/
+    public static String uploadFile(String fileUrl, String token,String fileName){
+        String id = null;
+        HashMap iheader = new HashMap<String, String>();
+        iheader.put("DEVICEID", DEVICEID);
+        iheader.put("DEVICESOURCE",DEVICESOURCE);
+        iheader.put("Lang",lang);
+        iheader.put("CEXTOKEN",token);
+        //从服务器获取图片资源
+        File file = FileUtil.getFileFromWeb(fileUrl+fileName);
+        try {
+            //调用上传图片接口，获取图片ID
+            Response response = OkHttpClientManager.post(ip+upLoadFileUrl,
+                    "multipart/form-data; boundary=----WebKitFormBoundarylsMUpMX3lOxQKla8", iheader,file,fileName);
+            if (response.code()==200) {
+               // System.out.println("body:"+response.body().string()+"header"+response.headers().toString());
+                JSONObject rspjson = JSON.parseObject(response.body().string());
+                if (rspjson.get("code").equals("000000")) {
+                    log.info("-------------Upload File success" + "body:"+rspjson.toJSONString()+ "\n");
+                    id = rspjson.getString("data");
+                } else {
+                    log.error("----------------Upload File failed, trace id is:" + rspjson.get("traceId") + "\n");
+                }
+            //    System.out.println("body:"+rspjson.toJSONString()+"header"+response.headers().toString());
+                return id;
+            }else {
+                log.error("----------------Server connect failed"+response.body()+"\n");
+                return id;
+            }
+
+        }catch (IOException e){
+            e.printStackTrace();
+            return id;
+        }
+
     }
 }
---
+++ src/main/java/io/cex/test/autotest/interfacecase/cex/CoreProcessTest.java
@@ -0,0 +1,73 @@
+package io.cex.test.autotest.interfacecase.cex;
+
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import io.cex.test.framework.assertutil.AssertTool;
+import io.cex.test.framework.common.RandomUtil;
+import io.cex.test.framework.httputil.OkHttpClientManager;
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.Response;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+/**
+ * @author shenqingyan
+ * @create 2019/8/8 11:25
+ * @desc 主流程测试类
+ **/
+@Slf4j
+public class CoreProcessTest extends BaseCase{
+    String token = null;
+    @BeforeClass
+    public void beforeClazz(){
+        //初始化header数据
+        dataInit();
+    }
+
+    @Test(description = "注册")
+    public void testRegister() throws IOException {
+        JSONObject object = new JSONObject();
+        object.put("identifier",randomPhone);
+        object.put("loginPwd",pwd);
+        object.put("mobileArea",area);
+        object.put("verifyCode","111111");
+        jsonbody.put("data",object);
+        Response response = OkHttpClientManager.post(ip+registerUrl, jsonbody.toJSONString(),
+                "application/json", header);
+        JSONObject rspjson = JSON.parseObject(response.body().string());
+        log.info("-------------register response is:"+rspjson.toJSONString());
+        AssertTool.isContainsExpect("success",rspjson.get("msg").toString());
+    }
+    @Test(dependsOnMethods = "testRegister",description = "登陆")
+    public void testLogin(){
+
+        token = BaseCase.userLogin(randomPhone,pwd,area);
+        AssertTool.assertNotEquals(null,token);
+        log.info("------------token:"+token);
+    }
+
+
+    @Test(dependsOnMethods = "testLogin",description = "身份认证")
+    public void testIdentity() throws IOException{
+        JSONObject object = new JSONObject();
+        object.put("countryId",countryId);
+        object.put("backId",BaseCase.uploadFile(fileUrl,token,"1.jpeg"));
+        object.put("frontId",BaseCase.uploadFile(fileUrl,token,"2.jpg"));
+        object.put("userName", RandomUtil.generateString(10));
+        object.put("certificateNo",RandomUtil.generateLong(18));
+        object.put("personId",BaseCase.uploadFile(fileUrl,token,"3.png"));
+        object.put("certificateType",certificateType);
+        jsonbody.put("data",object);
+        header.put("CEXTOKEN",token);
+        Response response = OkHttpClientManager.post(ip+identityUrl, jsonbody.toJSONString(),
+                "application/json", header);
+        JSONObject rspjson = JSON.parseObject(response.body().string());
+        System.out.printf("-------------Identity response is:"+rspjson);
+        AssertTool.isContainsExpect("success",rspjson.get("msg").toString());
+    }
+
+
+}
