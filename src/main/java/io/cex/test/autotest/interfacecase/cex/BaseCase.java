1273f9e751f2491959ca8894990437a3e0765f74
Anita <Qkx#oBRi!TP!833142>
2019-08-28 16:58:40 +0800

    loginoutTest

--- src/main/java/io/cex/test/autotest/interfacecase/cex/BaseCase.java
+++ src/main/java/io/cex/test/autotest/interfacecase/cex/BaseCase.java
@@ -84,7 +84,7 @@ public class BaseCase {
     public static final String querySymbolAsset = "/user/asset/query/symbolAsset";
     public static final String orderBookUrl = "/order/query/orderBook";
     public static final String symbolQuotationUrl = "/quotation/query/symbolQuotation";
-    public static final String loginoutTestUrl = "/user/logout";
+    public static final String loginoutUrl = "/user/logout";
     public static final String checkpwdTestUrl = "/user/password/check/securityPwd";
 
 
