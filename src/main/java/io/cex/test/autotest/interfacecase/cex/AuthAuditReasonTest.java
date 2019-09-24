c5a328bfd854341e8cf9c27cef5b26986a6639ea
liuxm <JI3QgmsWxyNK581034>
2019-09-24 17:47:44 +0800

    增加authAuditReason等相关接口测试

---
+++ src/main/java/io/cex/test/autotest/interfacecase/cex/AuthAuditReasonTest.java
@@ -0,0 +1,50 @@
+package io.cex.test.autotest.interfacecase.cex;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import io.cex.test.autotest.interfacecase.BaseCase;
+import io.cex.test.framework.assertutil.AssertTool;
+import io.cex.test.framework.httputil.OkHttpClientManager;
+import io.qameta.allure.Allure;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import okhttp3.Response;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+
+import static io.cex.test.autotest.interfacecase.cex.tool.CexConfig.*;
+import static io.cex.test.autotest.interfacecase.cex.tool.CexConfig.authAuditReasonUrl;
+@Feature("AuthAuditReason查询最后异常认证失败接口")
+
+public class AuthAuditReasonTest extends BaseCase {
+    @Test(description = "异常用例没有token，返回100006")
+    public void authAuditReasonTestError() throws IOException {
+        JSONObject jsonbody = new JSONObject();
+        jsonbody.put("lang",lang);
+        Response response = OkHttpClientManager.post(ip+authAuditReasonUrl, jsonbody.toJSONString(),
+                "application/json", BaseCase.dataInit());
+        JSONObject rspjson = JSON.parseObject(response.body().string());
+        Allure.addAttachment("入参：",jsonbody.toJSONString());
+        Allure.addAttachment("出参：",rspjson.toJSONString());
+        AssertTool.isContainsExpect("100006",rspjson.get("code").toString());
+    }
+    @Severity(SeverityLevel.CRITICAL)
+    @Test(description = "正常用例，返回000000")
+    public void authAuditReasonTest() throws IOException {
+        JSONObject jsonbody = new JSONObject();
+        jsonbody.put("lang",lang);
+        HashMap header = dataInit();
+        header.put("CEXTOKEN", presetToken);
+        Response response = OkHttpClientManager.post(ip+authAuditReasonUrl, jsonbody.toJSONString(),
+                "application/json", header);
+        JSONObject rspjson = JSON.parseObject(response.body().string());
+        Allure.addAttachment("入参：",jsonbody.toJSONString());
+        Allure.addAttachment("出参：",rspjson.toJSONString());
+        AssertTool.isContainsExpect("000000",rspjson.get("code").toString());
+    }
+
+}
